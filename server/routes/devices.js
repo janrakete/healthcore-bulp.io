0e21df2c42a67a09d5066a089c613c37e39895d1
janrakete <jan@scribe.de>
2025-07-21 23:36:56 +0200

    finished devices API

--- server/routes/devices.js
+++ server/routes/devices.js
@@ -4,10 +4,10 @@
  * ==================
  */
 
-const appConfig         = require("../../config");
-const router            = require("express").Router();
+const appConfig     = require("../../config");
+const router        = require("express").Router();
 
-const tablesAllowed  = appConfig.CONF_tablesAllowedForAPI; // defines, which tables are allowed
+const tablesAllowed = appConfig.CONF_tablesAllowedForAPI; // defines, which tables are allowed
 
 /**
  * This function builds an SQL statement for INSERT or UPDATE operations based on the provided payload.
@@ -23,24 +23,21 @@ async function statementBuild(table, payload, type="INSERT") {
    let response = {};
 
    const [results]   = await MySQLConnection.query("SHOW COLUMNS FROM " + mysqlConnection.escape(table)); // build an array with all fields of the table
-   let columnsList   = [];
-   for await (result of results) { 
-      columnsList.push(result.Field);
-   }
+   const columnsList = results.map(result => result.Field);
 
-   let data = [];
+   let dataList = [];
    
    if ((payload !== undefined) && (Object.keys(payload).length > 0)) {
-      for await (const [key, value] of Object.entries(payload)) { // loop through all keys of the JSON payload
+      for (const [key, value] of Object.entries(payload)) { // loop through all keys of the JSON payload
          if (columnsList.includes(key)) { // if key is an existing table column ... 
             response.status = "ok"; // ... return ok       
-            dataField       = {};
-            dataField[key]  = value;
-            data.push(dataField);
+            let data   = {};
+            data[key]  = value;
+            dataList.push(data);
          }
          else { // if key is not an existing table column ...
             response.status = "error"; // ... return error
-            response.error  = "Given key '" + key + "' does not exists in table";
+            response.error  = "Given column '" + key + "' does not exists in table";
             break;
          }
       }
@@ -50,9 +47,9 @@ async function statementBuild(table, payload, type="INSERT") {
          if (type === "INSERT") { // build INSERT statement
             let fields = "";
             let values = "";
-            for await (let data of data) {
+            for (let data of dataList) {
                fields = fields + Object.keys(data)[0] + ", "
-               values = values + "'" + data[Object.keys(data)[0]] + "', ";
+               values = values + mysqlConnection.escape(data[Object.keys(data)[0]]) + ", ";
             }
 
             fields = fields.substring(0, fields.length - 2);  // remove the last ", "
@@ -60,8 +57,8 @@ async function statementBuild(table, payload, type="INSERT") {
             response.statement = " (" + fields + ") VALUES (" + values + ")";
          }
          else { // build UPDATE statement
-            for await (let data of data) {
-               response.statement = response.statement + " " + Object.keys(data)[0] + "='" + data[Object.keys(data)[0]] + "', ";
+            for (let data of dataList) {
+               response.statement = response.statement + " " + Object.keys(data)[0] + "=" + mysqlConnection.escape(data[Object.keys(data)[0]]) + ", ";
             }
             response.statement = response.statement.substring(0, response.statement.length - 2);  // remove the last ", "
          }
@@ -74,38 +71,41 @@ async function statementBuild(table, payload, type="INSERT") {
    return (response);  
 }
 
-/*
-   Build WHERE condition out of payload
-*/
+/**
+ * This function builds a WHERE condition for SQL queries based on the provided payload.
+ * @async
+ * @function conditionBuild
+ * @param {string} table - The name of the table to build the condition for.
+ * @param {object} payload - The JSON payload containing the conditions to be applied.
+ * @returns {object} - An object containing the status of the operation, any error messages, and the constructed WHERE condition.
+ * @description This function checks if the keys in the payload match the columns of the specified table. If they do, it constructs a WHERE condition string for use in SQL queries. If any key does not match, it returns an error.
+ */
 async function conditionBuild(table, payload) {
-   let response              = {};
+   let response = {};
 
-   const [results]   = await MySQLConnection.query("SHOW COLUMNS FROM " + table); // build an array with all fields of the table
-   let columnsList = [];
-   for await (result of results) { 
-      columnsList.push(result.Field);
-   }
+   const [results]  = await MySQLConnection.query("SHOW COLUMNS FROM " + mysqlConnection.escape(table)); // build an array with all fields of the table
+   const columnsList = results.map(result => result.Field);
 
-   response.strCondition = "";   
+   response.condition = "";   
    if ((payload !== undefined) && (Object.keys(payload).length > 0)) {
-      for await (const [key, value] of Object.entries(payload)) { // loop through all keys of the JSON payload
+      for (const [key, value] of Object.entries(payload)) { // loop through all keys of the JSON payload
          if (columnsList.includes(key)) { // if key is an existing table column ...
-            response.status         = "ok"; // ... return ok and ...                
-            if (response.strCondition === undefined) {
-               response.strCondition = "";
+            response.status = "ok"; // ... return ok and ...                
+            if (response.condition === undefined) {
+               response.condition = "";
             }
-            response.strCondition = response.strCondition + " " + key + "='" + value + "' AND"; // ... build WHERE condition
+            response.condition = response.condition + " " + key + "=" + mysqlConnection.escape(value) + " AND"; // ... build WHERE condition
          }
          else { // if key is not an existing table column
-            response.strCondition = "";
-            response.status  = "error"; // ... return error
-            response.error   = "Given key '" + key + "' in condition block does not exists in table";
+            response.condition = "";
+            response.status    = "error"; // ... return error
+            response.error     = "Given column '" + key + "' in condition block does not exists in table";
             break;
          }
       }
       
-      if (response.strCondition != "") { // remove the last " AND"
-         response.strCondition = " WHERE " + response.strCondition.substring(0, response.strCondition.length - 4); 
+      if (response.condition !== "") { // remove the last " AND"
+         response.condition = " WHERE " + response.condition.substring(0, response.condition.length - 4); 
       }
    }  
    else {
@@ -115,31 +115,36 @@ async function conditionBuild(table, payload) {
    return (response);
 }
 
-/*
-   Create database entry
-*/
-router.post("/:table*", async function (Request, Response) {
-   const table    = Request.params.table;
-   let data          = {};
+/**
+ * Insert a new database entry or entries. If successful, it returns the last inserted ID.
+ * @route POST /:table*
+ * @param {string} table - The name of the table to insert data into.
+ * @param {object} payload - The JSON payload containing the data to be inserted.
+ * @returns {object} - An object containing the status of the operation, any error messages, and the last inserted ID if successful.
+ * @description This route allows clients to insert new entries into a specified table. It checks if the table name is allowed, builds an SQL INSERT statement based on the provided payload, and executes it. If successful, it returns the last inserted ID.
+ */
+router.post("/:table*", async function (request, response) {
+   const table = request.params.table;
+   let data    = {};
 
-   Request = Request.body;
+   request = request.body;
 
    if (tablesAllowed.includes(table)) {  // check, if table name is in allowed list
       try {
          data.status = "ok";
 
-         let Statement = await statementBuild(table, Request, "INSERT");
-         if (Statement.status === "ok") {
-            let statement = "INSERT INTO " + table + Statement.statement;
+         const statementBuild = await statementBuild(table, request, "INSERT");
+         if (statementBuild.status === "ok") {
+            const statement = "INSERT INTO " + table + statementBuild.statement;
             Common.conLog("POST request from client: access table '" + table + "'", "gre");
             Common.conLog("Execute statement: " + statement, "std", false);
 
             const result = await MySQLConnection.query(statement);
-            data.intID = result[0].insertId; // return last insert id
+            data.ID      = result[0].insertId; // return last insert id
          }
          else {
-            data.status = Statement.status;
-            data.error  = Statement.error;
+            data.status = statementBuild.status;
+            data.error  = statementBuild.error;
          }
       }
       catch (Error) {
@@ -153,29 +158,34 @@ router.post("/:table*", async function (Request, Response) {
    }
 
    if (data.status === "error") {
-      Common.conLog("Request from client: an error occured", "red");
+      Common.conLog("request from client: an error occured", "red");
    }
 
    Common.conLog("HTTP response: " + JSON.stringify(data), "std", false);
-   Response.json(data);
+   response.json(data);
 });
 
-/*
-   Get database entry or entries
-*/
-router.get("/:table*", async function (Request, Response) {
-   const table    = Request.params.table;
-   let data          = {};
+/**
+ * Retrieve database entries based on conditions.
+ * @route GET /:table*
+ * @param {string} table - The name of the table to retrieve data from.
+ * @param {object} query - The query parameters containing conditions for the retrieval.
+ * @returns {object} - An object containing the status of the operation, any error messages, and the retrieved results if successful.
+ * @description This route allows clients to retrieve entries from a specified table based on conditions provided in the query parameters. It checks if the table name is allowed, builds a WHERE condition based on the query parameters, and executes a SELECT statement. If successful, it returns the retrieved results.
+ */
+router.get("/:table*", async function (request, response) {
+   const table = request.params.table;
+   let data    = {};
 
-   Request = Request.query;
+   request = request.query;
 
    if (tablesAllowed.includes(table)) {  // check, if table name is in allowed list
       try {
          data.status = "ok";
 
-         let Condition = await conditionBuild(table, Request);
-         if (Condition.status === "ok") {
-            let statement     = "SELECT * FROM " + table + Condition.strCondition + " LIMIT 500";
+         const conditionBuild = await conditionBuild(table, request);
+         if (conditionBuild.status === "ok") {
+            const statement = "SELECT * FROM " + table + conditionBuild.condition + " LIMIT " + appConfig.CONF_tablesMaxEntriesReturned;
             Common.conLog("GET request client: access table '" + table + "'", "gre");
             Common.conLog("Execute statement: " + statement, "std", false);
 
@@ -183,8 +193,8 @@ router.get("/:table*", async function (Request, Response) {
             data.results      = results;
          }
          else {
-            data.status = Condition.status;
-            data.error  = Condition.error;
+            data.status = conditionBuild.status;
+            data.error  = conditionBuild.error;
          }
       }
       catch (Error) {
@@ -198,31 +208,36 @@ router.get("/:table*", async function (Request, Response) {
    }
 
    if (data.status === "error") {
-      Common.conLog("Request from client: an error occured", "red");
+      Common.conLog("request from client: an error occured", "red");
    }
 
    Common.conLog("HTTP response: " + JSON.stringify(data), "std", false);
-   Response.json(data);
+   response.json(data);
 });
 
 
-/*
-   Delete database entry or entries
-*/
-router.delete("/:table*", async function (Request, Response) {
-   const table    = Request.params.table;
-   let data          = {};
+/**
+ * Delete database entry or entries
+ * @route DELETE /:table*
+ * @param {string} table - The name of the table to delete data from.
+ * @param {object} query - The query parameters containing conditions for the deletion.
+ * @returns {object} - An object containing the status of the operation, any error messages and a confirmation message if successful.
+ * @description This route allows clients to delete entries from a specified table based on conditions provided in the query parameters. It checks if the table name is allowed, builds a WHERE condition based on the query parameters, and executes a DELETE statement. If successful, it returns a confirmation message.
+ */
+router.delete("/:table*", async function (request, response) {
+   const table = request.params.table;
+   let data    = {};
 
-   Request = Request.query;
+   request = request.query;
 
    if (tablesAllowed.includes(table)) {  // check, if table name is in allowed list
       try {
          data.status = "ok";
 
-         let Condition = await conditionBuild(table, Request);
-         if (Condition.status === "ok") {
-            if (Condition.strCondition.trim() != "") {
-               let statement     = "DELETE FROM " + table + Condition.strCondition + " LIMIT 1";
+         const conditionBuild = await conditionBuild(table, request);
+         if (conditionBuild.status === "ok") {
+            if (conditionBuild.condition.trim() !== "") {
+               const statement = "DELETE FROM " + table + conditionBuild.condition + " LIMIT 1";
                Common.conLog("DELETE request from client: access table '" + table + "'", "gre");
                Common.conLog("Execute statement: " + statement, "std", false);
       
@@ -234,8 +249,8 @@ router.delete("/:table*", async function (Request, Response) {
             }
          }
          else {
-            data.status = Condition.status;
-            data.error  = Condition.error;
+            data.status = conditionBuild.status;
+            data.error  = conditionBuild.error;
          }
       }
       catch (Error) {
@@ -249,42 +264,48 @@ router.delete("/:table*", async function (Request, Response) {
    }
 
    if (data.status === "error") {
-      Common.conLog("Request from client: an error occured", "red");
+      Common.conLog("request from client: an error occured", "red");
    }
 
    Common.conLog("HTTP response: " + JSON.stringify(data), "std", false);
-   Response.json(data);
+   response.json(data);
 });
 
-/*
-   Replace database entry or entries
-*/
-router.patch("/:table*", async function (Request, Response) {
-   const table    = Request.params.table;
-   let data          = {};
+/**
+ * Update database entry or entries
+ * @route PATCH /:table*
+ * @param {string} table - The name of the table to update data in.
+ * @param {object} body - The JSON payload containing the data to be updated. 
+ * @param {object} query - The query parameters containing conditions for the update.
+ * @returns {object} - An object containing the status of the operation, any error messages and a confirmation message if successful.
+ * @description This route allows clients to update entries in a specified table based on conditions provided in the query parameters. It checks if the table name is allowed, builds a WHERE condition based on the query parameters, constructs an SQL UPDATE statement from the provided body, and executes it. If successful, it returns a confirmation message.
+ */
+router.patch("/:table*", async function (request, response) {
+   const table = request.params.table;
+   let data    = {};
 
-   let RequestBody   = Request.body; // POST values are for data
-   let RequestQuery  = Request.query; // GET values are for condition
+   const requestBody  = request.body; // POST values are for data
+   const requestQuery = request.query; // GET values are for condition
 
    if (tablesAllowed.includes(table)) {  // check, if table name is in allowed list
       try {
          data.status = "ok";
 
-         let Condition = await conditionBuild(table, RequestQuery);
-         if (Condition.status === "ok") {
-            if (Condition.strCondition.trim() != "") {
+         const conditionBuild = await conditionBuild(table, requestQuery);
+         if (conditionBuild.status === "ok") {
+            if (conditionBuild.condition.trim() !== "") {
 
-               let Statement = await statementBuild(table, RequestBody, "UPDATE");
-               if (Statement.status === "ok") {
-                  let statement = "UPDATE " + table + " SET " + Statement.statement + Condition.strCondition + " LIMIT 1";
+               const statementBuild = await statementBuild(table, requestBody, "UPDATE");
+               if (statementBuild.status === "ok") {
+                  const statement = "UPDATE " + table + " SET " + statementBuild.statement + conditionBuild.condition + " LIMIT 1";
                   Common.conLog("PATCH request from client: access table '" + table + "'", "gre");
                   Common.conLog("Execute statement: " + statement, "std", false);
       
                   await MySQLConnection.query(statement);
                }
                else {
-                  data.status = Statement.status;
-                  data.error  = Statement.error;
+                  data.status = statementBuild.status;
+                  data.error  = statementBuild.error;
                }
             }
             else {
@@ -293,8 +314,8 @@ router.patch("/:table*", async function (Request, Response) {
             }
          }
          else {
-            data.status = Condition.status;
-            data.error  = Condition.error;
+            data.status = conditionBuild.status;
+            data.error  = conditionBuild.error;
          }
       }
       catch (Error) {
@@ -308,11 +329,11 @@ router.patch("/:table*", async function (Request, Response) {
    }
 
    if (data.status === "error") {
-      Common.conLog("Request from client: an error occured", "red");
+      Common.conLog("request from client: an error occured", "red");
    }
 
    Common.conLog("HTTP response: " + JSON.stringify(data), "std", false);
-   Response.json(data);
+   response.json(data);
 });
 
  module.exports = router;
