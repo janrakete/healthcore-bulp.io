20535506e160241c31bde1de1c2131c7d8f5dab6
janrakete <jan@scribe.de>
2025-08-19 23:25:12 +0200

    fixed anomaly detection

--- server/app.js
+++ server/app.js
@@ -92,22 +92,21 @@ async function startDatabaseAndServer() {
    */
   function anomalyCheck(data) { // TODO: convert to cron job
     const propertyKeys = data.properties.map(property => Object.keys(property)[0]);
-    propertyKeys.forEach((property, index) => {
-      const results = database.prepare(
+    propertyKeys.forEach((property, index) => { // iterate over each property
+      const results = database.prepare( // prepare SQL query to get historical data
         "SELECT valueAsNumeric FROM mqtt_devices_values WHERE deviceID = ? AND bridge = ? AND property = ? ORDER BY dateTimeAsNumeric DESC LIMIT ?"
       ).all(data.deviceID, data.bridge, property, appConfig.CONF_anomalyDetectionHistorySize);
 
-      if (!results || results.length < 2) {
+      if (!results || results.length < 2) { // not enough data for anomaly detection
         return;
       }
 
-      const values = results.map(result => { return { [property]: result.valueAsNumeric }} );
+      const values = results.map(result => { return { [property]: result.valueAsNumeric }} ); // map results to values  
       const model = new IsolationForest();
 
-      model.fit(values.slice(1));
+      model.fit(values.slice(1)); // Cut first entry of data, because if it is already in dataset then it will not be considered as anomaly
       const trainingScores = model.scores();
-      const latestScore    = model.predict([{ [property]: values[0][property] }])[0];
-
+      const latestScore    = model.predict([{ [property]: values[0][property] }])[0]; // get score of latest entry
       if (latestScore > appConfig.CONF_anomalyDetectionThreshold) {
         common.conLog("Server: Anomaly detected for property " + property + " with score " + latestScore, "gre");
         let message         = {};
