a56101c7f82d4498733d4207af66b647cca84c9c
janrakete <jan@scribe.de>
2025-10-05 08:55:58 +0200

    bangle js 2 test apps

---
+++ tests/devices/bulp-bangle-js/bulp.app.js
@@ -0,0 +1,14 @@
+var counter = 30;
+
+function countDown() {
+  counter--;
+
+  g.clear();
+  // draw the current counter value
+  g.drawString(counter, g.getWidth()/2, g.getHeight()/2);
+  // optional - this keeps the watch LCD lit up
+  Bangle.setLCDPower(1);
+}
+
+// call countDown every second
+var interval = setInterval(countDown, 1000);
