84417d787d1542303cf5b6868c412b7306c7509a
janrakete <jan@scribe.de>
2025-11-12 23:52:10 +0100

    app update

--- app/src/pages/Start.js
+++ app/src/pages/Start.js
@@ -6,38 +6,36 @@ import { toastShow } from "../services/toast.js";
 class Start extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <div class="ion-page">
-        <ion-header>
-          <ion-toolbar color="primary">
-            <ion-title>${window.Translation.get("PageStartHeadline")}</ion-title>
-          </ion-toolbar>
-        </ion-header>
-        <ion-content class="ion-padding">
+      <ion-header>
+        <ion-toolbar color="primary">
+          <ion-title>${window.Translation.get("PageStartHeadline")}</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content class="ion-padding">
 
-        <ion-grid>
-          <ion-row>
-            <ion-col>
-              <ion-img src="./src/assets/customer_logo_background.jpg"></ion-img>
-            </ion-col>
-          </ion-row>
-        </ion-grid>
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <ion-img src="./src/assets/customer_logo_background.jpg"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
 
-        <ion-grid>
-          <ion-row>
-            <ion-col><ion-button expand="block" shape="round"><ion-icon slot="start" name="notifications-sharp" size="large"></ion-icon> ${window.Translation.get("MessagesTitle")}</ion-button></ion-col>
-            <ion-col><ion-button expand="block" shape="round"><ion-icon slot="start" name="person-sharp" size="large"></ion-icon> ${window.Translation.get("PeopleTitle")}</ion-button></ion-col>
-          </ion-row>
-          <ion-row>
-              <ion-col><ion-button expand="block" shape="round"><ion-icon slot="start" name="scan-sharp" size="large"></ion-icon> ${window.Translation.get("RoomsTitle")}</ion-button></ion-col>
-              <ion-col><ion-button expand="block" shape="round"><ion-icon slot="start" name="radio-sharp" size="large"></ion-icon> ${window.Translation.get("DevicesTitle")}</ion-button></ion-col>
-          </ion-row>
-          <ion-row>
-            <ion-col><ion-button expand="block" shape="round"><ion-icon slot="start" name="unlink-sharp" size="large"></ion-icon> ${window.Translation.get("ScenariosTitle")}</ion-button></ion-col>
-            <ion-col><ion-button expand="block" shape="round"><ion-icon slot="start" name="build-sharp" size="large"></ion-icon> ${window.Translation.get("SettingsTitle")}</ion-button></ion-col>
-          </ion-row>
-        </ion-grid>
-        </ion-content>
-      </div>
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6"><ion-button expand="block" shape="round" ><ion-icon slot="start" name="notifications-sharp" size="large"></ion-icon> ${window.Translation.get("MessagesTitle")}</ion-button></ion-col>
+          <ion-col size="6"><ion-button expand="block" shape="round"><ion-icon slot="start" name="person-sharp" size="large"></ion-icon> ${window.Translation.get("PeopleTitle")}</ion-button></ion-col>
+        </ion-row>
+        <ion-row>
+            <ion-col size="6"><ion-button expand="block" shape="round"><ion-icon slot="start" name="scan-sharp" size="large"></ion-icon> ${window.Translation.get("RoomsTitle")}</ion-button></ion-col>
+            <ion-col size="6"><ion-button expand="block" shape="round"><ion-icon slot="start" name="radio-sharp" size="large"></ion-icon> ${window.Translation.get("DevicesTitle")}</ion-button></ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col size="6"><ion-button expand="block" shape="round"><ion-icon slot="start" name="unlink-sharp" size="large"></ion-icon> ${window.Translation.get("ScenariosTitle")}</ion-button></ion-col>
+          <ion-col size="6"><ion-button expand="block" shape="round"><ion-icon slot="start" name="build-sharp" size="large"></ion-icon> ${window.Translation.get("SettingsTitle")}</ion-button></ion-col>
+        </ion-row>
+      </ion-grid>
+      </ion-content>
     `;
     this.serverFind();
   }
