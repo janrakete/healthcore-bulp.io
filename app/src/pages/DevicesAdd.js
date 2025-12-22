688b6c1beb04b60064d7c34ea893c77c2615bf5d
janrakete <jan@scribe.de>
2025-12-22 18:15:10 +0100

    app: fixed some issues

--- app/src/pages/DevicesAdd.js
+++ app/src/pages/DevicesAdd.js
@@ -13,15 +13,15 @@ class DevicesAdd extends HTMLElement {
                 <ion-title>${window.Translation.get("PageDevicesAddHeadline")}</ion-title>
             </ion-toolbar>
         </ion-header>
-        <ion-content class="ion-padding">
+        <ion-content class="ion-padding background-1">
             <ion-grid>
                 <ion-row>
-                    <ion-col size="6"><ion-button expand="block" href="/devices-add/zigbee"><div><div><ion-img src="../assets/icons/bridges/icon_zigbee_1.svg"></ion-img><ion-text>${window.Translation.get("Zigbee")}</ion-text></div></div></ion-button></ion-col>
-                    <ion-col size="6"><ion-button expand="block" href="/devices-add/bluetooth"><div><div><ion-text>${window.Translation.get("Bluetooth")}</ion-text></div></div></ion-button></ion-col>
+                    <ion-col size="6"><ion-button expand="block" class="selection" href="/devices-add/zigbee"><div><div><ion-icon slot="start" name="icon-zigbee" size="large"></ion-icon></div><div><ion-text>${window.Translation.get("Zigbee")}</ion-text></div></div></div></ion-button></ion-col>
+                    <ion-col size="6"><ion-button expand="block" class="selection" href="/devices-add/bluetooth"><div><div><ion-icon slot="start" name="icon-bluetooth" size="large"></ion-icon></div><div><ion-text>${window.Translation.get("Bluetooth")}</ion-text></div></div></div></ion-button></ion-col>
                 </ion-row>
                 <ion-row>
-                    <ion-col size="6"><ion-button expand="block" href="/devices-add/lora"><div><div><ion-text>${window.Translation.get("LoRa")}</ion-text></div></div></ion-button></ion-col>
-                    <ion-col size="6"><ion-button expand="block" href="/devices-add/http"><div><div><ion-text>${window.Translation.get("Wifi")}</ion-text></div></div></ion-button></ion-col>
+                    <ion-col size="6"><ion-button expand="block" class="selection" href="/devices-add/lora"><div><div><ion-icon slot="start" name="icon-lora" size="large"></ion-icon></div><div><ion-text>${window.Translation.get("LoRa")}</ion-text></div></div></div></ion-button></ion-col>
+                    <ion-col size="6"><ion-button expand="block" class="selection" href="/devices-add/http"><div><div><ion-icon slot="start" name="icon-http" size="large"></ion-icon></div><div><ion-text>${window.Translation.get("Wifi")}</ion-text></div></div></div></ion-button></ion-col>
                 </ion-row>
             </ion-grid>
         </ion-content>
